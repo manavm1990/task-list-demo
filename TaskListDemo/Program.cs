e49cf960f91bde65679e416b994132d7b714fc1f
Manav Misra <manavm@visionwebsoft.com>
2022-03-11 13:44:53 -0600

    Log messages to show user choice from menu

--- TaskListDemo/Program.cs
+++ TaskListDemo/Program.cs
@@ -9,6 +9,29 @@ namespace TaskListDemo
             string[] tasks = new string[5];
 
             // Viewing should only show populated array slots
+            // Switch-case to respond to menu choices
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                switch (Select())
+                {
+                    case "1":
+                        Console.WriteLine("View List");
+                        break;
+                    case "2":
+                        Console.WriteLine("Add Task");
+                        break;
+                    case "3":
+                        Console.WriteLine("Remove A Task");
+                        break;
+                    default:
+                        isRunning = false;
+                        break;
+                }
+            }
+
+            // Add a task (option #2)
         }
 
         private static string Select()
