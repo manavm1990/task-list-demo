5cab2c71a39cd4195af79d5f75c3b621b7534fee
Manav Misra <manavm@visionwebsoft.com>
2022-03-11 13:14:07 -0600

    The array should be a string array and start with a size of 5

--- TaskListDemo/Program.cs
+++ TaskListDemo/Program.cs
@@ -6,7 +6,10 @@ namespace TaskListDemo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] tasks = new string[5];
+
+            // A menu should be displayed to allow a user to view, add, or remove items, or quit.
+            // Viewing should only show populated array slots
         }
     }
 }
