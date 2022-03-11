6c427dc1208b15fd949813fcf0e42cfdaa691094
Manav Misra <manavm@visionwebsoft.com>
2022-03-11 13:26:38 -0600

    A menu should be displayed to allow a user to view, add, or remove items, or quit

--- TaskListDemo/Program.cs
+++ TaskListDemo/Program.cs
@@ -4,12 +4,28 @@ namespace TaskListDemo
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main()
         {
             string[] tasks = new string[5];
 
-            // A menu should be displayed to allow a user to view, add, or remove items, or quit.
             // Viewing should only show populated array slots
+        }
+
+        private static string Select()
+        {
+            Console.Write(@"Task List
+=====================================
+1. View List
+2. Add a Task
+3. Remove a Task
+
+Press Q to quit
+=====================================
+
+Enter Choice:");
+
+            return Console.ReadLine();
+            // TODO: Compare the userChoice with what's in the menu
         }
     }
 }
